e8c7cd8aba2b871d1172b3c914280d05288cb5d6
Nicolás Gabioud <nicolasgabioud@hotmail.com>
2025-08-10 17:40:22 -0300

    Agrega Función de introducir amigos y mostrar la lista

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema
+// 
+
+let amigos = [];
+
+function mostrarAmigosEnLista () { 
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    amigos.forEach(nombre => {
+        const li = document.createElement("li");
+        li.textContent = nombre;
+        lista.appendChild(li);
+
+    console.log("Amigo agregado:", nombre);
+    }   
+    )
+    
+}
